539f64be339502e193ce2061342bf38b629b4e3b
22115wc <22115@wc.school.nz>
2025-02-24 19:09:01 +1300

    fixed up some bugs with input mins and maxs and the correct answer and got rid of some code comments

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ def quiz(questions):
     for q in questions[3:]:
         print(q)
     user_choice = askQ(questions[1],questions[2])
-    if user_choice==questions[1]:
+    if user_choice==questions[0]:
         print("Correct")
     else:
         print("Incorrect")
 
 #reused the below function from my assesment last year
-def askQ(minimum,maximum): #this is probably the most used function. It asks for user input and has all of the error detection you could possibly want. Whenever I ask a question where I need a number response from the user I use this function
+def askQ(minimum,maximum):
     print()
     loopBreak=True
     while loopBreak==True:
-        try: #this is a try part of a try-except statement. The program will attempt to execute the program in the try statement
+        try:
             userInput=int(input("Your choice: "))
             print()
             if userInput<minimum or userInput>maximum:
@@ -22,7 +22,7 @@ def askQ(minimum,maximum): #this is probably the most used function. It asks for
                 print()
             else:
                 loopBreak=False
-        except ValueError: #this is an except part of a try-except statement. If a ValueError occurs in the try statement (ie the user doesn't enter a number) this block of code will run. If no ValueError occurs this code is skipped
+        except ValueError:
             print()
             print("Please enter only numbers; no letters, spaces or special characters")
             print()
